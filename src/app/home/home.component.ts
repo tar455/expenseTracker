89075d53ecd05b6506da343ad104caabd1614f6d
Md Tarique Ansari <tariqueansari455@gmail.com>
2024-04-28 21:40:50 +0530

    v1:4 Added componet design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { OverviewComponent } from '../overview/overview.component';
+import { ExpensestatComponent } from '../expensestat/expensestat.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent,OverviewComponent,ExpensestatComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
